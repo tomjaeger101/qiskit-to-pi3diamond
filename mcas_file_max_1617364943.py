aa99a872bc2139f719fabad006dfeac61b3547b8
tomjaeger101 <90648477+tomjaeger101@users.noreply.github.com>
2023-02-24 13:27:29 +0100

    Update mcas_file_max_1617364943.py

--- mcas_file_max_1617364943.py
+++ mcas_file_max_1617364943.py
@@ -12,23 +12,31 @@ nuclear = sch.create_nuclear(__file__)
 with open(os.path.abspath(__file__).split('.')[0] + '.py', 'r') as f: 
 	meas_code = f.read() 
  
+'''
+here we create the multi channel sequence. For details see operations.py
+'''
+
 def ret_ret_mcas(pdc):
 	def ret_mcas(current_iterator_df):
 		mcas = MCAS.MultiChSeq(seq_name=seq_name, ch_dict={'2g': [1, 2], '128m': [1, 2]})
 		for idx, _I_ in current_iterator_df.iterrows():
 
 			op.full_initialisation(mcas, '+++')
-			op.readout_nuclear_spin_state(mcas, '+++')
-			
+			op.rx_nitrogen(mcas, 3.14159265358979, ms=0)
+			op.ry_nitrogen(mcas, 3.14159265358979, ms=0)
+			op.ry_nitrogen(mcas, 3.14159265358979, ms=0)
+			op.rx_nitrogen(mcas, 3.14159265358979, ms=0)
+			op.readout_nuclear_spin_state(mcas, '+++', step_idx=2)
+
 			pi3d.gated_counter.set_n_values(mcas)
 		return mcas
 	return ret_mcas
  
 def settings(pdc={}):
 	ana_seq=[
-	['init', '<', 0, 0, 10, 2],
-	['init', '<', 0, 0, 10, 1],
-	['result', '<', 0, 0, 10, 2],
+	['init', '<', 0, 0, 10, 2], #initial readout threshold
+	['init', '>', 3, 0, 0, 1],  #csd threshold
+	['result', '<', 0, 0, 10, 2], #final readout threshold
 	]
 
 	sch.settings(
@@ -42,18 +50,24 @@ def settings(pdc={}):
 	nuclear.analyze_type = 'standard'
 
 	pi3d.gated_counter.trace.analyze_type = 'standard'
-	pi3d.gated_counter.trace.consecutive_valid_result_numbers = [0]
-	pi3d.gated_counter.trace.average_results = True
+	#pi3d.gated_counter.trace.consecutive_valid_result_numbers = [0]
+	pi3d.gated_counter.trace.average_results = False
 
+	nuclear.odmr_interval = 1
+ 	nuclear.refocus_interval = 1
+	nuclear.maximum_odmr_drift = 0.01               #maximum drift can be increased for C414 and Nitrogen 
+	nuclear.refocus_moving_average_factor = 1
 	nuclear.number_of_simultaneous_measurements = 1
 
 	nuclear.parameters = OrderedDict(
 		(
-			('sweeps', range(30)),
+			('sweeps', range(20)),
+			('theta', [0]),
+			('state_result', ['+++']),
 		)
 	)
 def run_fun(abort, **kwargs):
-	pi3d.readout_duration = 150e6
+	pi3d.gated_counter.readout_duration = 20e6
 	nuclear.debug_mode = False
 	settings()
 	nuclear.run(abort)
